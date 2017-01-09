4e01963f3fb1e54e8e80dfd48c8878c2b2f41750
Jonathan Aca <jonathanaca22@gmail.com>
2017-01-09 22:25:09 +0000

    I made a smiley face

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Write your function calls below this line
+setColor(0, 0, "red");
+setColor(9, 0, "red");
+setColor(9, 9, "red");
+setColor(0, 9, "red");
+
+setColor(3, 3, "blue");
+setColor(3, 6, "blue");
+setColor(6, 2, "blue");
+setColor(7, 4, "blue");
+setColor(7, 5, "blue");
+setColor(6, 7, "blue");
